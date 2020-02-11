d6c03feec7a30d0508258ca2de9dc157dc6b59fb
Victor Tran <victor.n.tran@outlook.com>
2020-02-10 23:49:29 -0500

    create user model

---
+++ data/models/user.js
@@ -0,0 +1,36 @@
+const db = require("../knex.config");
+
+const table = "users";
+
+const find = () => {
+  return null;
+};
+
+const findById = id => {
+  return null;
+};
+
+const findByUsername = username => {
+  return null;
+};
+
+const update = (id, changes) => {
+  return null;
+};
+
+const add = model => {
+  return null;
+};
+
+const remove = id => {
+  return null;
+};
+
+module.exports = {
+  find,
+  findById,
+  findByUsername,
+  update,
+  add,
+  remove
+};
