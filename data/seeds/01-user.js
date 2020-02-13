c60de41a7a8a5c4daf90dedc253ba6cfbeca4266
Victor Tran <victor.n.tran@outlook.com>
2020-02-12 19:31:22 -0500

    update seed values

--- data/seeds/01-user.js
+++ data/seeds/01-user.js
@@ -2,33 +2,33 @@ exports.seed = async function(knex) {
   await knex("users").insert([
     {
       id: 1,
-      first: "Victor",
-      last: "Tran",
-      email: "victor.n.tran@outlook.com",
-      username: "ivictoris",
+      first: "blah",
+      last: "bloh",
+      email: "blah@outlook.com",
+      username: "iblahis",
       password: "$2a$13$zsZrin2LIC0UdAuK.gIG4e2B/jkyl9nFC8./d.YO5OAMleUtSgY3y"
     },
     {
       id: 2,
-      first: "Victor",
-      last: "Tran",
-      email: "victor.n.tran.social@outlook.com",
-      username: "ivictor",
+      first: "blah",
+      last: "bloh",
+      email: "blah.social@outlook.com",
+      username: "iblah",
       password: "$2a$13$zsZrin2LIC0UdAuK.gIG4e2B/jkyl9nFC8./d.YO5OAMleUtSgY3y"
     },
     {
       id: 3,
-      first: "Victor",
-      last: "Tran",
-      email: "victor.n.tran.game@outlook.com",
+      first: "blah",
+      last: "bloh",
+      email: "blah.game@outlook.com",
       username: "iviktor",
       password: "$2a$13$zsZrin2LIC0UdAuK.gIG4e2B/jkyl9nFC8./d.YO5OAMleUtSgY3y"
     },
     {
       id: 4,
-      first: "Victor",
-      last: "Tran",
-      email: "victor.n.tran.subscription@outlook.com",
+      first: "blah",
+      last: "bloh",
+      email: "blah.subscription@outlook.com",
       username: "iviktoris",
       password: "$2a$13$zsZrin2LIC0UdAuK.gIG4e2B/jkyl9nFC8./d.YO5OAMleUtSgY3y"
     }
