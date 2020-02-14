889641e3e0b0fdb97cf9b85cfc1fad08a29b64b0
Victor Tran <victor.n.tran@outlook.com>
2020-02-14 17:25:59 -0500

    add root / get path

--- api/server.js
+++ api/server.js
@@ -4,4 +4,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.status(200).json({ api: "running" });
+});
+
 module.exports = app;
