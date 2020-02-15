2fbaafbf62d2687e3a098638814d12050625f6c6
Victor Tran <victor.n.tran@outlook.com>
2020-02-14 21:41:24 -0500

    add user route

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 
+const { router: userRouter } = require("../router/userRouter");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/users", userRouter);
 app.get("/", (req, res) => {
   res.status(200).json({ api: "running" });
 });
-
 module.exports = app;
